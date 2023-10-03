51003e16da3696cbf55db305b85058773d08bc5b
Ajai Maxwel <axavierjames@postmedia.com>
2023-10-02 22:59:37 -0400

    Added readme

---
+++ README.md
@@ -0,0 +1,43 @@
+Certainly! Here's a README file for your project:
+
+---
+
+# Wave Effect Video Generator
+
+This program generates a video with a wave effect applied to an input image. The wave effect simulates a rippling motion that intensifies towards the bottom of the image.
+
+## How it Works
+
+The program uses OpenCV to read an input image and creates a video by applying a sinusoidal wave pattern to the image. As the video progresses, the phase of the wave changes, creating a rippling effect.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- OpenCV (`pip install opencv-python`)
+
+### How to Run
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/ajaimaxwel-pmd/python_wave_generator
+   ```
+
+2. Navigate to the directory:
+   ```bash
+   cd python_wave_generator
+   ```
+
+3. Run the program:
+   ```bash
+   python3 main.py
+   ```
+
+### Input and Output
+
+- **Input Image**: `lake.jpg` (Replace with your preferred image)![lake.jpg](lake.jpg)
+- **Output Video**: `output_video.mp4` (The generated video with the wave effect applied) ![output_video_to-gif.gif](output_video_to-gif.gif)
+## Acknowledgment
+
+Special thanks to the photographer, **Quang Nguyen Vinh**, for the beautiful image that was used in this project. 
--- main.py
+++ main.py
@@ -4,27 +4,41 @@ import numpy as np
 
 def apply_wave_effect(img, phase, wave_amplitude):
     rows, cols, _ = img.shape
-    wave_img = np.copy(img)
-    for y in range(int(rows * 0.70), rows):
-        for x in range(cols):
-            # gradient
-            if y >= int(rows * 0.80):
-                wave_amplitude = 2.5
-            offset_y = int(wave_amplitude * np.sin(2 * np.pi * x / 180 + phase))
-            if 0 <= y + offset_y < rows:
-                wave_img[y + offset_y, x] = img[y, x]
+
+    # Create a grid of x, y coordinates
+    x = np.arange(cols)
+    y = np.arange(rows)
+    X, Y = np.meshgrid(x, y, indexing='xy')
+    print(X,Y)
+
+    # Calculate the wave pattern
+    wave = wave_amplitude * np.sin(2 * np.pi * X / 180 + phase)
+
+    # Update Y values based on masks and cast to int32
+    mask_70_80 = (Y >= int(rows * 0.70)) & (Y < int(rows * 0.80))
+    mask_80_above = Y >= int(rows * 0.80)
+
+    Y[mask_70_80] += wave[mask_70_80].astype(np.int32)
+    Y[mask_80_above] += (2.5 * wave[mask_80_above]).astype(np.int32)
+
+    # Clip to ensure Y indices remain within the image dimensions
+    Y = np.clip(Y, 0, rows - 1)
+
+    # Using fancy indexing to get the final image
+    wave_img = img[Y, X]
+
     return wave_img
 
 
-image_path = "pexels-quang-nguyen-vinh-2166695.jpg"
-output_video_path = "output_video.mp4"
+image_path = "pexels-quang-nguyen-vinh-2166695 (4).jpg"
+output_video_path = "output_video_main_wave10.mp4"
 
 img = cv2.imread(image_path)
 h, w, _ = img.shape
 fps = 30
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(output_video_path, fourcc, fps, (w, h))
-wave_height = 5.0
+wave_height = 10.0
 phase = 0.0
 
 for _ in range(5 * fps):
@@ -33,3 +47,4 @@ for _ in range(5 * fps):
     phase -= 0.04
 
 out.release()
+
